66bd71c61965064c2bccd5d86d6365d07c85622e
NIEZEMA <gmgoilandgas@gmail.com>
2022-10-27 19:56:18 +0100

    tickting

---
+++ profile/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5qaX4vJpYfjBQ8s9FEMX77Q4D7beTlts",
+  authDomain: "profile-5c3c9.firebaseapp.com",
+  projectId: "profile-5c3c9",
+  storageBucket: "profile-5c3c9.appspot.com",
+  messagingSenderId: "1044140001313",
+  appId: "1:1044140001313:web:ff699b8ede4c68aa55bb60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
